2f5c271c43e01a567f22cb9755dec6ccd56f4794
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-04-21 00:36:54 +0000

    defer to C#11

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# Create the file '/tmp/school'
-file { '/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# kills a process named killmenow.
-exec { 'kills killmenow.':
-  command => 'killall killmenow',
-  path    => '/usr/bin'
-}
-
