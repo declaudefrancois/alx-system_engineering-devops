eae19690d19900eb145176dc6f0159a73b88721a
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-06-23 17:29:03 -0700

    (0x0A): puppet file resource

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Define a file ressource with content, mode, user...
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
