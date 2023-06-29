0c1ec3f0cfe9dc3a45a88ba9321294801b39e870
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-06-29 02:25:44 +0100

    (0x0C): fix nginx auto setup scripts

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,21 @@
 # Install & setup Nginx web server usign puppet
-package { 'nginx',
+package { 'nginx':
   ensure  => installed
 }
 
-$content = "server {
-       		listen 80 default;
-
-		rewrite ^/redirect_me$ https://www.youtube.com permanent;
-		location = \ {
-			return 200 "Hello World!";
-		}
-	}
-        "
-
 file { '/etc/nginx/sites-available/default':
-  content => $content,
+  content => "server {\n\tlisten 80 default;\n\trewrite ^/redirect_me\\/?$ https://www.youtube.com permanent;\n\tlocation = / {\n\t\treturn 200 \"Hello World!\\n\";\n\t}\n}",
   require => Package['nginx']
 }
 
-exec {'Reload nginx'
+exec {'Restart nginx':
   command => '/usr/sbin/service nginx restart',
+  user    => root,
+  require => Package['nginx']
+}
+
+exec {'Reload nginx':
+  command => '/usr/sbin/nginx -s reload',
+  user    => root,
   require => Package['nginx']
 }
